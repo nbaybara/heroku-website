8971e077e0b6cbfe778d0458af4496c33cc0f4c6
Nur Sefa BAYBARA <nsbaybar@gmail.com>
2020-10-26 01:59:27 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,11 +4,17 @@ import os
 import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
